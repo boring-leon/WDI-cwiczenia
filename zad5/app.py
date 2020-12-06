117a1453ba6c8ce94b8e98ee39343eb3ed2f8cd6
Leon Czerwinski <poriusz@wp.pl>
2020-12-06 17:31:27 +0100

    zad5

---
+++ zad5/app.py
@@ -0,0 +1,21 @@
+def findMax(arr):
+    throwIfEmpty(arr)
+    max = arr[0]
+    for el in arr:
+        if el > max: 
+            max = el
+    
+    return max
+
+def throwIfEmpty(arr):
+    if len(arr) == 0:
+        raise ValueError("Provided sequence can't be empty")
+
+
+while True:
+    try:
+        stringSequence = str(input("Input your sequence number: "))
+        numSequence = [int(n) for n in stringSequence.split()]
+        print(findMax(numSequence))
+    except ValueError as exception:
+        print(str(exception))
